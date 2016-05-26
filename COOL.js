156773ce31588aba38e0bef895fbcbd2eabfcbbf
Rosanne Hu <rhu2@wellesley.edu>
2016-05-26 12:35:50 +0800

    Things might fly soon

---
+++ COOL.js
@@ -0,0 +1,29 @@
+/*
+FILE NAME: COOL.js
+WRITTEN BY: ROSANNE  HUHUHUHUHUHU
+DATE: today is probably like the may of like 26 fam
+PURPOSE: There is no purpose in life
+*/
+
+$(document).ready(function(){
+    animateDiv();
+
+});
+
+function Look
+function MOVINGMYLIFEAROUND(){
+    var height = $(window).height()-50;
+    var width = $(window).width()-50;
+
+    var newheight = Math.floor(Math.random() * height))
+    var newwitdth = Math.floor(Math.random() * width))
+
+    return [newheight,newwidth]
+}
+
+function MAkeTHisMofofly(){
+    var NEWYeaR = MOVINGMYLIFEAROUND();
+    $(#clickme).animate({ top: newq[0], left: newq[1] },function(){
+      animateDiv();
+    });
+}
